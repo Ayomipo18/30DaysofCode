adc9d7594362ebf69b06d374dea4fc7479887024
Ayomipo Solaja <ayomiposolaja@gmail.com>
2020-05-02 22:06:37 +0100

    day 2 done

---
+++ Day2.js
@@ -0,0 +1,9 @@
+const single = (num) => {
+	let times = 0;
+	num = num.toString();
+	while (num.length > 1) {
+        times++;
+        num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+    }
+	return times;
+}
